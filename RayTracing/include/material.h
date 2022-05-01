51a98702d6766648864e1d4fb5ebcc88d70d12ea
Pierce_qiang <2579404335@qq.com>
2022-05-01 21:02:22 +0800

    next 7.5

--- RayTracing/include/material.h
+++ RayTracing/include/material.h
@@ -88,3 +88,22 @@ private:
         return r0 + (1 - r0) * pow((1 - cosine), 5);
     }
 };
+
+class diffuse_light : public material {
+public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const override {
+        return false;
+    }
+
+    virtual color emitted(double u, double v, const point3& p) const override {
+        return emit->value(u, v, p);
+    }
+
+public:
+    shared_ptr<texture> emit;
+};
